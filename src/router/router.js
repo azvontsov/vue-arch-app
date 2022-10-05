c315cff26c8150ddbf68a2a1488824fddadb8b8e
anton <azvontsov@gmail.com>
2022-10-05 10:13:57 -0400

    createWebHashHistory

--- src/router/router.js
+++ src/router/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import Home from "../pages/Home.vue";
 
 const routes = [
@@ -46,7 +46,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
   linkActiveClass: "active-link",
   scrollBehavior(to, from, savedPosition) {
